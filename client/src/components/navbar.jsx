27cc00e6f940b38cb31cb35ad872bef3a5f30eab
bayurn <bayurizkynurseto19@gmail.com>
2020-08-03 22:59:36 +0700

    adding navbar

---
+++ client/src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div class="ui inverted vertical pointing menu">
+            <a class="active item">
+                Home
+            </a>
+            <a class="item">
+                Messages
+            </a>
+            <a class="item">
+                Friends
+            </a>
+        </div>
+    )
+};
+
+export default Navbar;
