8216c2dc25fb07ecdf1ccdae3431c9541e25a8f6
bayurn <bayurizkynurseto19@gmail.com>
2020-08-05 00:01:54 +0700

    resolve menu

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-import { Menu } from 'semantic-ui-react'
-import 'semantic-ui-css/semantic.min.css'
+import { Menu, Container, Sidebar } from 'semantic-ui-react'
 
 export default class Navbar extends Component {
     state = { activeItem: 'home' }
@@ -11,12 +10,14 @@ export default class Navbar extends Component {
         const { activeItem } = this.state
 
         return (
-            <Menu inverted vertical pointing>
-                <Menu.Item name='Home' active={activeItem === 'Home'} onClick={this.handleItemClick} />
-                <Menu.Item name='Messages' active={activeItem === 'Messages'} onClick={this.handleItemClick} />
-                <Menu.Item name='Friends' active={activeItem === 'Friends'} onClick={this.handleItemClick} />
-                <Menu.Menu/>
-            </Menu>
+            <Container style={{ margin: 20 }}>
+                <Menu inverted vertical pointing>
+                    <Menu.Item name='Home' active={activeItem === 'Home'} onClick={this.handleItemClick} />
+                    <Menu.Item name='Messages' active={activeItem === 'Messages'} onClick={this.handleItemClick} />
+                    <Menu.Item name='Friends' active={activeItem === 'Friends'} onClick={this.handleItemClick} />
+                    <Menu.Menu />
+                </Menu>
+            </Container>
         )
     }
 };
