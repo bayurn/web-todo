ccaaee83e95f33723f0c554b21107f98ef7bf5ad
bayurn <bayurizkynurseto19@gmail.com>
2020-08-06 23:14:00 +0700

    change file name

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Navbar from './components/navbar'
+import Sidebar from './components/sidebar'
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <Sidebar />
     </div>
   );
 }
--- client/src/components/sidebar.js
+++ client/src/components/sidebar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import { Menu, Container, Sidebar } from 'semantic-ui-react'
+import { Menu } from 'semantic-ui-react'
 
-export default class Navbar extends Component {
+export default class Sidebar extends Component {
     state = { activeItem: 'home' }
 
     handleItemClick = (e, { name }) => this.setState({ activeItem: name })
@@ -10,14 +10,15 @@ export default class Navbar extends Component {
         const { activeItem } = this.state
 
         return (
-            <Container style={{ margin: 20 }}>
+            <div style={{ minHeight: '100vh', display: 'flex', flexFlow: 'column nowrap' }}>
                 <Menu inverted vertical pointing>
                     <Menu.Item name='Home' active={activeItem === 'Home'} onClick={this.handleItemClick} />
                     <Menu.Item name='Messages' active={activeItem === 'Messages'} onClick={this.handleItemClick} />
                     <Menu.Item name='Friends' active={activeItem === 'Friends'} onClick={this.handleItemClick} />
                     <Menu.Menu />
                 </Menu>
-            </Container>
+            </div>
+
         )
     }
 };
