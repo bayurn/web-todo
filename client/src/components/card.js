16e835e44146c1cdea0c12f348d5ff4a9e53e89c
bayurn <bayurizkynurseto19@gmail.com>
2020-08-09 00:01:38 +0700

    add form

---
+++ client/src/components/card.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Card, Icon } from 'semantic-ui-react'
+
+function TagCard() {
+    const description = [
+        'Amy is a violinist with 2 years experience in the wedding industry.',
+        'She enjoys the outdoors and currently resides in upstate New York.',
+    ].join(' ')
+    return (
+        <Card>
+            <Card.Content header='About Amy' />
+
+            <Card.Content description={description} />
+            <Card.Content extra>
+                <Icon name='user' />4 Friends
+            </Card.Content>
+        </Card>
+    )
+}
+
+export default TagCard
