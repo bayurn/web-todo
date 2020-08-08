40ab07489c05db99ff5ec9dc3ccde0519fcc3dbc
bayurn <bayurizkynurseto19@gmail.com>
2020-08-08 23:37:58 +0700

    add card

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,12 +1,23 @@
 import React from 'react'
-import { Sidebar, Header, Segment } from 'semantic-ui-react'
+import { Sidebar, Header, Segment, Card, Icon } from 'semantic-ui-react'
 
 function Home() {
-    return (
+    const description = [
+        'Amy is a violinist with 2 years experience in the wedding industry.',
+        'She enjoys the outdoors and currently resides in upstate New York.',
+    ].join(' ')
 
+    return (
         <Sidebar.Pusher>
             <Segment basic>
                 <Header as='h3'>WebApp Todo</Header>
+                <Card>
+                    <Card.Content header='About Amy' />
+                    <Card.Content description={description} />
+                    <Card.Content extra>
+                        <Icon name='user' />4 Friends
+                    </Card.Content>
+                </Card>
             </Segment>
         </Sidebar.Pusher>
     )
