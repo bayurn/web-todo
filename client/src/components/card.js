518344cb8c1605b96419e1e590ff1d2215609fd2
bayurn <bayurizkynurseto19@gmail.com>
2020-08-09 23:37:16 +0700

    add table

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Card, Icon } from 'semantic-ui-react'
+import { Card } from 'semantic-ui-react'
 import Forms from './form'
 
 function TagCard() {
-    const description = [
-        'Amy is a violinist with 2 years experience in the wedding industry.',
-        'She enjoys the outdoors and currently resides in upstate New York.',
-    ].join(' ')
+    // const description = [
+    //     'Amy is a violinist with 2 years experience in the wedding industry.',
+    //     'She enjoys the outdoors and currently resides in upstate New York.',
+    // ].join(' ')
 
     return (
         <div >
