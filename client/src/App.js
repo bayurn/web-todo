0567096bffe1a2424970492e63f9b4bc4add5346
bayurn <bayurizkynurseto19@gmail.com>
2020-08-14 22:30:39 +0700

    add route attrib path and component add

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Router, Switch, Route} from 'react-router-dom'
 import Home from './components/home';
+import Add from './components/add';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route path="/add" component={Add} />
       </Switch>
     </Router>
     );
