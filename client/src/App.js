e23085a854e55c633e3645fdc9c08a6eb71eaac3
bayurn <bayurizkynurseto19@gmail.com>
2020-08-14 22:38:17 +0700

    add router attrib history

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Router, Switch, Route} from 'react-router-dom'
+import { Router, Switch, Route } from 'react-router-dom'
 import Home from './components/home';
 import Add from './components/add';
+import history from './history'
 
 function App() {
   return (
-    <Router>
+    <Router history={history}>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/add" component={Add} />
       </Switch>
     </Router>
-    );
+  );
 }
 
 export default App;
