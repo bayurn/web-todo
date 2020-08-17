317a204a0c999999c5a53f89218b7a302fabc57a
bayurn <bayurizkynurseto19@gmail.com>
2020-08-17 23:41:43 +0700

    modified home.js

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Sidebar, Header, Segment } from 'semantic-ui-react'
 import Tables from './table'
+import SideBar from './sidebar'
 
 function Home() {
     return (
-        <Sidebar.Pusher>
-            <Segment basic>
-                <Header as='h3'>WebApp Todo</Header>
-                <Tables />
-            </Segment>
-        </Sidebar.Pusher>
+        <>
+            <SideBar />
+            <Sidebar.Pusher>
+                <Segment basic>
+                    <Header as='h3'>WebApp Todo</Header>
+                    <Tables />
+                </Segment>
+            </Sidebar.Pusher>
+        </>
     )
 }
 
