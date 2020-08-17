a58619dce2045d7902edfb6dce5489744f0402bc
bayurn <bayurizkynurseto19@gmail.com>
2020-08-17 23:42:23 +0700

    modified index.js

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ import App from './App';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <SideBar />
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
